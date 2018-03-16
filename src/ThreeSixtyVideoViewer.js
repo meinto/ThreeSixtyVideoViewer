bbe1ca17b374ac7d87808872bd2d7efecf845375
Armin Fix <armin.fix@gmx.de>
2018-03-16 17:10:42 +0100

    create minimum value library package

---
+++ src/ThreeSixtyVideoViewer.js
@@ -0,0 +1,43 @@
+import Hammer from 'hammerjs';
+
+class ThreeSixtyVideoViewer {
+  constructor(settings) {
+    const {
+      selector = '#video', boost = 6, frameRate = 6, pixelToFrame = 6,
+    } = settings;
+
+    this.video = document.querySelector(selector); // eslint-disable-line
+    this.hammer = new Hammer(this.video);
+
+    let lastStep = 0;
+    let step = 0;
+
+    this.hammer.add(new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10 }));
+
+    this.hammer.on('pan', (ev) => {
+      step = Math.floor(ev.deltaX / pixelToFrame);
+    });
+
+    this.hammer.on('panleft', (ev) => {
+      if (step !== lastStep && !this.video.seeking) {
+        this.video.currentTime -= (1 + -ev.velocityX * boost) / frameRate; // eslint-disable-line
+        lastStep = step;
+        if (this.video.currentTime === 0) {
+          this.video.currentTime = this.video.duration;
+        }
+      }
+    });
+
+    this.hammer.on('panright', (ev) => {
+      if (step !== lastStep && !this.video.seeking) {
+        this.video.currentTime += (1 + ev.velocityX * boost) / frameRate; // eslint-disable-line
+        lastStep = step;
+        if (this.video.duration === this.video.currentTime) {
+          this.video.currentTime = 0;
+        }
+      }
+    });
+  }
+}
+
+module.exports = settings => new ThreeSixtyVideoViewer(settings);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+const path = require('path');
+const config = require('./package.json');
+const env = require('yargs').argv.env;
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const plugins = [];
+
+module.exports = {
+  entry: path.resolve(__dirname, config.main),
+  devtool: 'source-map',
+  mode: env === 'build' ? 'production' : 'development',
+  output: {
+    library: pkg.name,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: env === 'build' ? 'ThreeSixtyVideoViewer.min.js' : 'ThreeSixtyVideoViewer.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: plugins,
+};
